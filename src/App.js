0044fb30d3f3f18148a6e5723568fd206cd3d59b
radha-soni <er.radha17@gmail.com>
2020-04-15 16:53:09 +0530

    infinite scroll added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Images from "./components/Images";
 import axios from "axios";
 
 function App() {
-  const [movies, setMovies] = useState(null);
-  const [page, setPage] = useState(3);
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, settotalPages] = useState(5);
+  const [scrolling, setScrolling] = useState(false);
 
   useEffect(() => {
     getMoviesData();
+    window.addEventListener("scroll", (e) => handleScroll(e));
   }, [page]);
 
   function getMoviesData() {
@@ -17,18 +20,39 @@ function App() {
         `https://api.themoviedb.org/3/discover/movie?api_key=0ca2f8b99d329e0ef9376fcddadf56ed&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
       )
       .then((response) => {
-        const movies = response.data.results;
-        setMovies(movies);
+        const updatedMovies = response.data.results;
+        // settotalPages(response.data.totalPages);
+
+        setMovies((movies) => {
+          return [...movies, updatedMovies];
+        });
+        setScrolling(false);
       });
   }
   function loadMore() {
     setPage(page + 1);
+    setScrolling(true);
+  }
+  useEffect(() => {
+    console.log(movies);
+  }, [movies]);
+
+  function handleScroll(e) {
+    const bottomOffset = 20;
+    const pageOffset = window.innerHeight + window.scrollY;
+    const setHeight = document.body.offsetHeight - bottomOffset;
+    if (scrolling) return;
+    if (totalPages <= page) {
+      return;
+    }
+    if (pageOffset >= setHeight) {
+      loadMore();
+    }
   }
 
   return (
     <>
       <div className="App">
-        <button onClick={loadMore}>Load More</button>
         {movies ? <Images movies={movies} /> : "loading"}
       </div>
     </>
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,25 +1,25 @@
 import React from "react";
+import "./images.css";
 
 export default function Images({ movies }) {
-  if (movies) {
-    console.log(movies);
-  }
   return (
-    <>
-      {movies
-        ? movies.map((element) => {
+    <div className="grid-container">
+      {movies &&
+        movies.map((element) =>
+          element.map((ele) => {
             return (
-              <div key={element.id}>
+              <div key={ele.id}>
                 <img
-                  src={"https://image.tmdb.org/t/p/w300" + element.poster_path}
+                  src={"https://image.tmdb.org/t/p/w300" + ele.poster_path}
                   alt="movie-name"
                 />
-                <p>{element.title}</p>
-                <p>{element.overview}</p>
+                <p>{ele.title}</p>
+
+                {/* <p>{ele.overview}</p> */}
               </div>
             );
           })
-        : "loading"}
-    </>
+        )}
+    </div>
   );
 }
